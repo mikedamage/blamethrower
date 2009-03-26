117704618156f8e67da2aa23a280f216f5fe953a
Mike Green <mike.is.green@gmail.com>
2009-03-26 14:01:28 -0400

    updated database schema for Blame model

--- db/migrations/setup_blames.rb
+++ db/migrations/setup_blames.rb
@@ -2,7 +2,8 @@ class SetupBlames < ActiveRecord::Migration
 	def self.up
 		create_table :blames do |t|
 			t.integer :user_id
-			t.integer :target_user
+			t.integer :target_user_id
+			t.string :target_user_name
 			t.string	:reason
 			t.timestamps
 		end
