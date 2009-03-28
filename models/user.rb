2bc8cab628a2ce7575ea5c2d072ee6042d95013c
Mike Green <mike.is.green@gmail.com>
2009-03-28 16:03:48 -0400

    added some validations to user model

--- models/user.rb
+++ models/user.rb
@@ -1,6 +1,12 @@
 class User < ActiveRecord::Base
 	has_many :blames
 	
+	validates_presence_of :name, :on => :create
+	validates_presence_of :email, :on => :create, :message => "can't be blank"
+	validates_length_of :password, :within => 4..100, :on => :create, :message => "must be at least 4 characters long"
+	validates_uniqueness_of :name, :on => :create, :message => "already exists"
+	
+	
 	def self.authenticate(user, pass)
 		current_user = self.find(:first, :conditions => {:name => user})
 		return nil if current_user.nil?
