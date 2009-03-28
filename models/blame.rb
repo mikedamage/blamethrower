c45e16ff54584c4b29e0066fd788822e1d1f1a14
Mike Green <mike.is.green@gmail.com>
2009-03-28 15:56:41 -0400

    added target user name finder method to before_save on blames

--- models/blame.rb
+++ models/blame.rb
@@ -1,3 +1,8 @@
 class Blame < ActiveRecord::Base
 	belongs_to :user
+	
+	def before_save
+		target_user_name = User.find(:first, :conditions => {:id => this.target_id}).name
+		this.target_name = target_user_name
+	end
 end
